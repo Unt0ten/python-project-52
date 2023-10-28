7da12e6f7a85402f79cc7a182bd1612c98892537
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-10-28 12:07:33 +0700

    deploy

--- Makefile
+++ Makefile
@@ -1,5 +1,18 @@
+HOST ?= 127.0.0.1
+PORT ?= 8000
+WEB_CONCURRENCY ?= 4
+
 dev:
 	poetry run python manage.py runserver
 
 shell:
 	poetry run python manage.py shell
+
+migrate:
+	poetry run python manage.py migrate
+
+migrations:
+	python manage.py makemigrations
+
+start:
+	poetry run gunicorn -w $(WEB_CONCURRENCY) -b $(HOST):$(PORT) task_manager.wsgi:application
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,18 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+LOCAL_HOST = os.getenv('HOST')
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'webserver',
+    LOCAL_HOST,
+]
+
+RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -46,6 +57,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
