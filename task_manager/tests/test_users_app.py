50d62f3f8c1934a26bb43c3a7d2f5d86c91cd992
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-11-11 10:21:43 +0700

    add test app users

--- task_manager/tests/test_users_app.py
+++ task_manager/tests/test_users_app.py
@@ -1,15 +1,19 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.shortcuts import reverse
+from django.core.exceptions import ObjectDoesNotExist
 
 from task_manager.users.models import CustomUser
-from task_manager.users import forms
 
 
 class StatusCodeTestCase(TestCase):
 
-    def SetUp(self):
-        CustomUser.objects.create(username="lion", password="111")
-        self.user = CustomUser.objects.last()
+    def setUp(self):
+        CustomUser.objects.create(username='lion', password='111')
+        CustomUser.objects.create(username='user', password='123')
+        self.logged_user = CustomUser.objects.get(username='lion')
+        self.anonim_user = CustomUser.objects.get(username='user')
+        self.anonim_user_pk = self.anonim_user.id
+        self.logged_user_pk = self.logged_user.id
 
     def test_users_url(self):
         response = self.client.get('/users/')
@@ -28,25 +32,37 @@ class StatusCodeTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_change_url_auth(self):
-        # поправить тесты!
-        user_pk = self.user.id
-        self.client._login(self.user)
-        response = self.client.get(f'/users/{user_pk}/update/')
+        self.client._login(self.logged_user)
+        response = self.client.get(f'/users/{self.logged_user_pk}/update/')
         self.assertEqual(response.status_code, 200)
 
-    # def test_change_url_anonim(self):
-    #     CustomUser.objects.create(username="lion", password="111")
-    #     user = CustomUser.objects.last()
-    #     user_pk = user.id
-    #     self.client._login(user)
-    #     response = self.client.get(f'/users/{user_pk}/update/')
-    #     self.assertEqual(response.status_code, 200)
+    def test_change_url_anonim(self):
+        response = self.client.get(f'/users/{self.logged_user_pk}/update/')
+        self.assertRedirects(response, '/login/')
+
+    def test_delete_url_auth(self):
+        self.client._login(self.logged_user)
+        response = self.client.get(f'/users/{self.logged_user_pk}/delete/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_url_anonim(self):
+        response = self.client.get(f'/users/{self.anonim_user_pk}/delete/')
+        self.assertRedirects(response, '/login/')
+
+    def test_permissions_url_change(self):
+        self.client._login(self.logged_user)
+        response = self.client.get(f'/users/{self.anonim_user_pk}/update/')
+        self.assertRedirects(response, '/users/')
+
+    def test_permissions_url_delete(self):
+        self.client._login(self.logged_user)
+        response = self.client.get(f'/users/{self.anonim_user_pk}/delete/')
+        self.assertRedirects(response, '/users/')
 
 
-class UsersCRUDTestCase(TestCase):
+class UsersCUDTestCase(TestCase):
 
-    def SetUp(self):
-        self.client = Client()
+    def setUp(self):
         self.data = {
             'valid_user': {
                 'username': '@user',
@@ -55,13 +71,12 @@ class UsersCRUDTestCase(TestCase):
                 'password1': '111',
                 'password2': '111'
             },
-            'logged_valid_user': {
-                'username': '@user',
-                'first_mame': 'Ivan',
-                'last_name': 'Ivanov',
-                'password1': '111',
-                'password2': '111',
-                'is_authenticated': 'True'
+            'updated_valid_user': {
+                'username': '@master',
+                'first_mame': 'iVAN',
+                'last_name': 'Ivanoff',
+                'password1': '123',
+                'password2': '123',
             },
             'wrong_user_username': {
                 'username': '$user',
@@ -77,16 +92,57 @@ class UsersCRUDTestCase(TestCase):
                 'password1': '111',
                 'password2': '112'
             },
-            'anonimus_user': {
-                'username': '',
-                'first_mame': '',
-                'last_name': '',
-                'password1': '',
-                'password2': '',
-                'is_anonymous': 'True',
-            },
         }
 
-    def test_users_view(self):
-        pass
+        self.user_data = {
+            'first_name': 'Ivan',
+            'last_name': 'Ivanov',
+            'username': 'vanishe',
+            'password': '111',
+            }
+
+    def test_create_user_success(self):
+        url = reverse('create_user')
+        user = self.data['valid_user']
+        response = self.client.post(url, user)
+        self.assertRedirects(response, '/login/')
+        find_user = CustomUser.objects.get(username='@user')
+        self.assertEqual(find_user.username, '@user')
+
+    def test_create_user_false_username(self):
+        url = reverse('create_user')
+        wrong_user_username = self.data['wrong_user_username']
+        self.client.post(url, wrong_user_username)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            CustomUser.objects.get(username='$user')
+
+    def test_create_user_false_password(self):
+        url = reverse('create_user')
+        wrong_user_password = self.data['wrong_user_password']
+        self.client.post(url, wrong_user_password)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            CustomUser.objects.get(username='@user')
+
+    def test_update_user(self):
+        user = CustomUser.objects.create(**self.user_data)
+        self.client._login(user)
+        user_pk = user.id
+        url = reverse('update_user', kwargs={'pk': user_pk})
+        self.client.post(url, self.data['updated_valid_user'])
+        update_user = CustomUser.objects.get(id=user_pk)
+        self.assertEqual(update_user.username, '@master')
+
+    def test_delete_user(self):
+        user = CustomUser.objects.create(**self.user_data)
+        self.client._login(user)
+        user_pk = user.id
+        url = reverse('delete_user', kwargs={'pk': user_pk})
+        self.client.post(url)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            CustomUser.objects.get(id=user_pk)
+
+
 
