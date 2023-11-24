6f607cedc9e08915f282b6d984fa751229c753f7
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-11-24 22:16:27 +0700

    fix form_valid

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -59,8 +59,24 @@ class UserFormDeleteView(
     success_url = reverse_lazy('users')
 
     def form_valid(self, form):
-        messages.success(self.request, _('User deleted successfully!'))
-        return super().form_valid(form)
+        try:
+            messages.success(self.request, _('User deleted successfully'))
+            return super().form_valid(form)
+        except ProtectedError:
+            messages.warning(
+                    self.request,
+                    _('Cannot delete user because it is in use')
+                )
+            return redirect('users')
+        # try:
+        #     messages.success(self.request, _('User deleted successfully!'))
+        #     return super().form_valid(form)
+        # except ProtectedError:
+        #     messages.warning(
+        #             self.request,
+        #             _('Cannot delete user because it is in use')
+        #         )
+        #     return redirect('users')
 
 
 
