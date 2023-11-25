80a4ffc803b5d295e0a3e688acc5dc8808df268f
Alexandr Sakhatov <sakhatoff@gmail.com>
2023-11-25 19:03:45 +0700

    tests

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ poetry install
 
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py makemigrations
-#poetry run python manage.py migrate
+poetry run python manage.py migrate
 make compile
